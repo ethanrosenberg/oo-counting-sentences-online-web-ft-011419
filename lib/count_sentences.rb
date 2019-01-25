c2cf4d830a2a8ac7e263ca204dba321d612d6115
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-25 06:04:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    splits1 = self.split(".")
+    splits2 = self.split("?")
+    splits3 = self.split("!")
+    binding.pry
   end
 end
